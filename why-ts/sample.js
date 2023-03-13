95073fc18cd28ba94004b6751e076f3efb3bf666
hjkim-tg <hjkim@teamgrit.kr>
2023-03-13 17:05:05 +0900

    타입스크립트 소개와 배경

---
+++ why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// 자바스크립트를 타입스크립트처럼 코딩하는 방법
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+
+// sum(10, "20");
